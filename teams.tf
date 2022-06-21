8b667a109a9097493f5f78a6486e2c1e451c2605
Carl Javier <carl.javier@hashicorp.com>
2022-06-21 10:41:49 +1000

    Use workspace module

--- teams.tf
+++ teams.tf
@@ -1,3 +1,6 @@
+# Workspaces created using the basic modules will create workspaces but won't have all the bells and whistles. 
+# If you need more control and want to set more workspace settings, suggest using a terraform team and creating 
+# workspaces with another module. See workspaces.tf for examples.
 module "aws-team" {
   source         = "./modules/teams"
   teamname       = "aws-sharedservices"
@@ -29,4 +32,11 @@ module "app-service" {
   workspaces     = ["app-service-dev", "app-service-iam-profiles", "app-service-domain-names"]
   workspace-tags = ["aws", "app-service"]
   organization   = data.tfe_organization.teamcarljavier.name
+}
+
+# The following is an example of only creating a Team with no workspaces
+# TFC Admins would look to create workspaces seperately. See workspaces.tf for examples 
+resource "tfe_team" "teamnoworkspaces" {
+  name         = "teamnoworkspaces"
+  organization = data.tfe_organization.teamcarljavier.name
 }
--- workspaces.tf
+++ workspaces.tf
@@ -12,3 +12,29 @@ resource "tfe_workspace" "appB" {
   tag_names    = ["test", "app"]
 }
 
+
+# Example using https://registry.terraform.io/modules/alexbasista/workspacer/tfe module
+# Seperating out your workspace creation makes sense as this way, you can add more workspace settings
+module "teamappX-workspace" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.2.0"
+  # insert the 7 required variables here
+
+  organization   = data.tfe_organization.teamcarljavier.name
+  workspace_name = "App-Workspace-Dev"
+  workspace_desc = "Description of my new TFE Workspace."
+  workspace_tags = ["experiment", "dev", "sharedservice"]
+
+  terraform_version = "1.1.9"
+
+  team_access = {
+    "azure-sharedservices-users" = "plan"
+    "app-service-users"          = "read"
+  }
+
+  tfvars = {
+    teststring = "iamstring"
+    testlist   = ["1", "2", "3"]
+    testmap    = { "a" = "1", "b" = "2", "c" = "3" }
+  }
+}
